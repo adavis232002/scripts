da6c1751ef29e07747412201644543e5441164d9
adavis232002 <andrew@adavis.us>
2014-03-28 08:05:55 -0500

    Added function to stop service and wait for it to shutdown.

--- reboot_bdr.ps1
+++ reboot_bdr.ps1
@@ -7,7 +7,10 @@ if($ProcessActive -eq $null)
     $BDR_Type_StorageCraft = Test-Path "C:\Program Files (x86)\StorageCraft\ShadowProtect\ShadowProtectSvc.exe"
     if($BDR_Type_Replay4 -eq "True")
     {
-        Write-Host BDR is Replay 4
+        #Stop Replay Core service, wait for it to shutdown, and send ok to reboot status back to Kaseya
+        #Stop-Service ReplayServer64
+        fncCheckService -ServiceName "wudfsvc"
+
     }
     elseif($BDR_Type_AppAssure5 -eq "True")
     {
@@ -25,4 +28,16 @@ if($ProcessActive -eq $null)
 else
 {
 	Write-host User Logged On
+}
+
+function fncCheckService{
+param($ServiceName)
+$arrService = Get-Service -Name $ServiceName
+$i = 0
+write-host $i
+while(($arrService.Status -eq "Running") -or ($i -lt 1))
+{
+write-host "Service is still running"
+$counter++
+}
 }
