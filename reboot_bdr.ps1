f8040ceff7351eab85c3c32bdd9fa6f86ba6fae8
adavis232002 <andrew@adavis.us>
2014-04-16 10:53:25 -0500

    Updated script to not do anything app assure 5 and set timeout at 2 minutes with a check every 15 seconds.

--- reboot_bdr.ps1
+++ reboot_bdr.ps1
@@ -1,17 +1,16 @@
 function fncCheckService{
 param($ServiceName)
 
-$timeout = new-timespan -Minutes 1
+$timeout = new-timespan -Minutes 2
 $sw = [diagnostics.stopwatch]::StartNew()
 while ($sw.elapsed -lt $timeout){
     $arrService = Get-Service -Name $ServiceName
-	write-host $arrService.Status
 	if ($arrService.Status -eq "Stopped"){
 		write-host "Service has been stopped"
         return
         }
  
-    start-sleep -seconds 5
+    start-sleep -seconds 15
 }
  
 write-host "Service did not stop in a timely manner"
@@ -22,18 +21,14 @@ if($ProcessActive -eq $null)
 {
 	#Determine type of BDR for safe shutdown by looking if the service executable exists
     $BDR_Type_Replay4 = Test-Path "C:\Program Files (x86)\AppAssure Software\Replay Server\Server64\TevoTarget.exe"
-    $BDR_Type_AppAssure5 = Test-Path "C:\Program Files\AppRecovery\Core\CoreService\Core.Service.exe"
     $BDR_Type_StorageCraft = Test-Path "C:\Program Files (x86)\StorageCraft\ShadowProtect\ShadowProtectSvc.exe"
     if($BDR_Type_Replay4 -eq "True")
     {
         #Stop Replay Core service, wait for it to shutdown, and send ok to reboot status back to Kaseya
         #Stop-Service ReplayServer64
+		Stop-Service TermService
         fncCheckService -ServiceName "TermService"
 
-    }
-    elseif($BDR_Type_AppAssure5 -eq "True")
-    {
-        Write-Host BDR is AppAssure 5
     }
     elseif($BDR_Type_StorageCraft -eq "True")
     {
